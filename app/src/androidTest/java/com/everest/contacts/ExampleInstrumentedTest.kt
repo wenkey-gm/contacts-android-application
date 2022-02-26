5e94f67bd5e9d8f9787ded2732a4310690eee8d3
wenkey <muni.v.g@everest.engineering>
2022-02-26 13:04:06 +0530

    Added Simple case for values in edit field

--- app/src/androidTest/java/com/everest/contacts/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/everest/contacts/ExampleInstrumentedTest.kt
@@ -1,12 +1,15 @@
 package com.everest.contacts
 
-import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
-
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-
-import org.junit.Assert.*
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -15,10 +18,29 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+
+    @get:Rule
+    val rule = ActivityScenarioRule(MainActivity::class.java)
+
+
+
     @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.everest.contacts", appContext.packageName)
+    fun should_display_values_in_second_activity() {
+        //enter the text
+        onView(withId(R.id.contactNameOne)).perform(typeText("wenkey"), closeSoftKeyboard())
+        onView(withId(R.id.contactNumberOne)).perform(typeText("1234"), closeSoftKeyboard())
+        onView(withId(R.id.contactNameTwo)).perform(typeText("kutty"), closeSoftKeyboard())
+        onView(withId(R.id.contactNumberTwo)).perform(typeText("45678"), closeSoftKeyboard())
+
+        //button is clicked
+        onView(withId(R.id.save_button)).perform(click())
+
+        //No need to tell espresso, while activity is changed
+        onView(withId(R.id.contactName1)).check(matches(withText("wenkey")))
+        onView(withId(R.id.phoneNumber1)).check(matches(withText("1234")))
+        onView(withId(R.id.contactName2)).check(matches(withText("kutty")))
+        onView(withId(R.id.phoneNumber2)).check(matches(withText("45678")))
     }
+
+
 }
